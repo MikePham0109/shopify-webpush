595a4fcad8532a48c13004fca9048f3c06fb2df1
Peter Vo <vtphat19@gmail.com>
2022-05-22 17:12:34 +0700

    update on redirect

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='root_path'),
+    # for more redirect's usage options: https://docs.djangoproject.com/en/2.1/topics/http/shortcuts/
+    path('static/service-wokers/ssp-sw.js/', lambda request: redirect('/static/service-wokers/ssp-sw.js', permanent=True)),
 ]
