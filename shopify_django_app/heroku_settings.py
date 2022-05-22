d099dfe43da79243d0a0fe767a5c5f0cfe3c7239
Peter Vo <vtphat19@gmail.com>
2022-05-22 15:58:25 +0700

    update

--- shopify_django_app/heroku_settings.py
+++ shopify_django_app/heroku_settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from shopify_app import *
-from postgresify import postgresify
+# from postgresify import postgresify
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +86,10 @@ WSGI_APPLICATION = 'shopify_django_app.wsgi.application'
 #     }
 # }
 
-DATABASES = postgresify()
+# DATABASES = postgresify()
+
+django_on_heroku.settings(locals())
+
 
 
 # Password validation
