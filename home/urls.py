8553c48372191dbf36b701a30f221905e7e99218
Peter Vo <vtphat19@gmail.com>
2022-05-22 17:20:21 +0700

    update on trailing slash issue

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='root_path'),
-    # for more redirect's usage options: https://docs.djangoproject.com/en/2.1/topics/http/shortcuts/
-    path('static/service-wokers/ssp-sw.js/', lambda request: redirect('/static/service-wokers/ssp-sw.js', permanent=True)),
 ]
